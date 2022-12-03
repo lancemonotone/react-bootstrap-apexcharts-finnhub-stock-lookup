bbd9e476511f1826196c3e8ac71874cced5d59e7
Rus Miller <lancemonotone@gmail.com>
2022-12-03 13:27:30 -0500

    Removed base from Vite config to debug display.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/react-bootstrap-apexcharts-finnhub-stock-lookup/',
+  // base: '/react-bootstrap-apexcharts-finnhub-stock-lookup/',
   plugins: [react()]
 })
