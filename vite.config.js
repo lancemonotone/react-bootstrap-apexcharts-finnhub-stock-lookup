a436f4e7aa20793f1042823cc8318bd1b9a865b1
Rus Miller <lancemonotone@gmail.com>
2022-12-03 12:48:34 -0500

    Added repo base to Vite config.

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/react-bootstrap-apexcharts-finnhub-stock-lookup/',
   plugins: [react()]
 })
